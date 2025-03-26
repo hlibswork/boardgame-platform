5538e29cfe6e697eb74e71119521a6f354eb1078
Koziakov Hlib <hlibs.work@gmail.com>
2025-03-26 03:57:29 +0200

    All models were implemented according to the scheme.

--- boardgame_platform/settings.py
+++ boardgame_platform/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "boardgames"
+    "boardgames",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "boardgames.Player"
--- boardgames/models.py
+++ boardgames/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class BoardGame(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    genre = models.CharField(max_length=255)
+    min_players = models.IntegerField()
+    max_players = models.IntegerField()
+    description = models.TextField()
+    image_url = models.URLField()
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class Player(AbstractUser):
+    class Meta:
+        ordering = ("username",)
+
+    HOST_PLAYER_CHOICES = (
+        ('Player', 'Player'),
+        ('Host', 'Host'),
+    )
+    role = models.CharField(max_length=6, choices=HOST_PLAYER_CHOICES, default="Player")
+    favorite_games = models.ManyToManyField(BoardGame, related_name="favorited_by")
+
+    def __str__(self):
+        return f"Player {self.username} with {self.role} role."
+
+
+class Event(models.Model):
+    class Meta:
+        ordering = ["date"]
+        unique_together = ("title", "date")
+
+    title = models.CharField(max_length=255)
+    host = models.ForeignKey(Player, on_delete=models.CASCADE, related_name="hosted_events")
+    game = models.ForeignKey(BoardGame, on_delete=models.CASCADE, related_name="events")
+    date = models.DateTimeField()
+    location = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.title} on {self.date.strftime('%Y-%m-%d %H:%M')}"
+
+
+class Registration(models.Model):
+    class Meta:
+        unique_together = ("player", "event")
+
+    REGISTER_CHOICES = (
+        ('Registered', 'Registered'),
+        ('Canceled', 'Canceled'),
+    )
+    player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name="registrations")
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="registrations")
+    status = models.CharField(max_length=10, choices=REGISTER_CHOICES, default="Registered")
+    registered_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Player {self.player.username} was {self.status} on {self.event}."
