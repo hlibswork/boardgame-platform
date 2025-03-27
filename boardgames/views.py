5b385aa214f3ecd8df236ba335f17927e969a21d
Koziakov Hlib <hlibs.work@gmail.com>
2025-03-27 22:54:31 +0200

    Display live platform stats on homepage (games, players, events)

--- boardgames/views.py
+++ boardgames/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+from boardgames.models import BoardGame, Player, Event
+
 
 def index(request):
-    return render(request, "index.html")
+    num_games = BoardGame.objects.count()
+    num_players = Player.objects.count()
+    num_events = Event.objects.count()
+
+    context = {
+        "num_games": num_games,
+        "num_players": num_players,
+        "num_events": num_events,
+    }
+    return render(request, "index.html", context=context)
