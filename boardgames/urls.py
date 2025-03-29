859673989e8bdb988c6a089071de5930362cd804
Koziakov Hlib <hlibs.work@gmail.com>
2025-03-29 20:17:19 +0200

    feat: added BoardGame, Event and Player ListViews with templates and pagination

--- boardgames/urls.py
+++ boardgames/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from boardgames.views import index
+
+from boardgames.views import index, BoardGameListView, EventListView, PlayerListView
 
 urlpatterns = [
-   path("", index)
+    path("", index),
+    path("boardgames/", BoardGameListView.as_view(), name="board-game-list"),
+    path("events/", EventListView.as_view(), name="event-list"),
+    path("players/", PlayerListView.as_view(), name="player-list")
 ]
--- boardgames/views.py
+++ boardgames/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.views import generic
 
 from boardgames.models import BoardGame, Player, Event
 
@@ -14,3 +16,24 @@ def index(request):
         "num_events": num_events,
     }
     return render(request, "index.html", context=context)
+
+
+class BoardGameListView(generic.ListView):
+    model = BoardGame
+    context_object_name = "boardgames"
+    paginate_by = 5
+
+
+class EventListView(generic.ListView):
+    model = Event
+    context_object_name = "events"
+    paginate_by = 5
+
+    def get_queryset(self):
+        return Event.objects.filter(date__gte=timezone.now()).order_by("date")
+
+
+class PlayerListView(generic.ListView):
+    model = Player
+    context_object_name = "players"
+    paginate_by = 5
