e2cf75a0378e5b65f469a9c8b6c02e40747a7f22
Koziakov Hlib <hlibs.work@gmail.com>
2025-04-05 20:53:42 +0300

    fix: add trailing slashes to all URL patterns

--- boardgames/urls.py
+++ boardgames/urls.py
@@ -23,23 +23,23 @@ urlpatterns = [
     path("events/", EventListView.as_view(), name="event-list"),
     path("players/", PlayerListView.as_view(), name="player-list"),
     path(
-        "boardgames/<int:pk>", BoardGameDetailView.as_view(), name="board-game-detail"
+        "boardgames/<int:pk>/", BoardGameDetailView.as_view(), name="board-game-detail"
     ),
-    path("boardgames/create", BoardGameCreateView.as_view(), name="board-game-create"),
+    path("boardgames/create/", BoardGameCreateView.as_view(), name="board-game-create"),
     path(
-        "boardgames/update/<int:pk>",
+        "boardgames/update/<int:pk>/",
         BoardGameUpdateView.as_view(),
         name="board-game-update",
     ),
     path(
-        "boardgames/delete/<int:pk>",
+        "boardgames/delete/<int:pk>/",
         BoardGameDeleteView.as_view(),
         name="board-game-delete",
     ),
-    path("events/<int:pk>", EventDetailView.as_view(), name="event-detail"),
-    path("events/create", EventCreateView.as_view(), name="event-create"),
+    path("events/<int:pk>/", EventDetailView.as_view(), name="event-detail"),
+    path("events/create/", EventCreateView.as_view(), name="event-create"),
     path("events/update/<int:pk>", EventUpdateView.as_view(), name="event-update"),
-    path("events/delete/<int:pk>", EventDeleteView.as_view(), name="event-delete"),
+    path("events/delete/<int:pk>/", EventDeleteView.as_view(), name="event-delete"),
     path("events/<int:pk>/toggle/", toggle_assign_to_event, name="event-toggle"),
-    path("my_events", RegisteredEventsByUserListView.as_view(), name="my-events"),
+    path("my_events/", RegisteredEventsByUserListView.as_view(), name="my-events"),
 ]
