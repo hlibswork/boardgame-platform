e573902d5e065bf621548315181007f14f7377e6
Koziakov Hlib <hlibs.work@gmail.com>
2025-04-13 16:23:11 +0300

    refactor: restructure project config and split settings into base/dev/prod

---
+++ config/settings/dev.py
@@ -0,0 +1,20 @@
+from .base import *
+
+
+import os
+
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
+ALLOWED_HOSTS = ["127.0.0.1"]
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for boardgame_platform project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
